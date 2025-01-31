f6156be2052dac8b085ec14ecafd1d1c76b7f68f
Diegospadotto <165823930+Diegospadotto@users.noreply.github.com>
2025-01-31 13:22:59 -0300

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,1 +1,8 @@
+import cv2
 
+def load_image(image_path):
+    """Carrega uma imagem usando OpenCV."""
+    image = cv2.imread(image_path)
+    if image is None:
+        print(f"Erro ao carregar imagem: {image_path}")
+    return image
